0d16b2f39b7437e7a3a5398c136a295b4218f29f
Sathvik Talabathula <mail2sathvik67@gmail.com>
2026-07-18 18:54:04 +0530

    Update java

---
+++ Java.java
@@ -0,0 +1,11 @@
+public class Java{
+    public static void main(String[] args){
+        String name="Sathvik";
+        int id=170;
+        char grade='A';
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(grade);
+
+    }
+}
